0af4c29875df66c419b0b6570b671e2530febcb0
Caleb Kugel <calebkugel1@gmail.com>
2024-12-11 01:57:50 -0600

    finished day 1

---
+++ src/DayOne.cpp
@@ -0,0 +1,5 @@
+//
+// Created by calebk on 12/1/24.
+//
+
+#include "DayOne.h"
---
+++ src/DayOne.h
@@ -0,0 +1,14 @@
+//
+// Created by calebk on 12/1/24.
+//
+
+#ifndef ADVENTOFCODE2024_DAYONE_H
+#define ADVENTOFCODE2024_DAYONE_H
+
+
+class DayOne {
+
+};
+
+
+#endif //ADVENTOFCODE2024_DAYONE_H
